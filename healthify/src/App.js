12608bb45ac28ccc365a182d640df626b1f09a4e
Khalil Alausa <alausakhalil@gmail.com>
2025-05-25 05:51:47 -0900

    hope page

--- healthify/src/App.js
+++ healthify/src/App.js
@@ -1,14 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
-import HeroSection from './components/HeroSection';
-import Footer from './components/Footer';
-import Quality from './components/Qaulity';
-import HealthInsuranceHero from './components/HealthInsurance';
-import EveryonesHealth from './components/EveryonesHealth';
-import TeleHealthFeatures from './components/TeleHealthFeatures';
-import DoctorSection from './components/DoctorSection';
-import ServicesSection from './components/HealthService';
-import TestimonialsSection from './components/TestimonialsSection';
+import logo from "./logo.svg";
+import "./App.css";
+import HeroSection from "./components/HeroSection";
+import Footer from "./components/Footer";
+import Quality from "./components/Qaulity";
+import HealthInsuranceHero from "./components/HealthInsurance";
+import EveryonesHealth from "./components/EveryonesHealth";
+import TeleHealthFeatures from "./components/TeleHealthFeatures";
+import DoctorSection from "./components/DoctorSection";
+import ServicesSection from "./components/HealthService";
+import TestimonialsSection from "./components/TestimonialsSection";
 
 const App = () => {
   return (
